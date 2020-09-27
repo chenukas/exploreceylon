ad04721eb26414ab8b299f6c2e0fb14bf57e7eaa
unknown <chenukasumanasekara@gmail.com>
2020-09-27 17:26:59 +0530

    Fixed issues

--- src/screens/leGrand.js
+++ src/screens/leGrand.js
@@ -7,15 +7,30 @@ import {
   StyleSheet,
   ScrollView,
   Image,
+  Dimensions,
 } from "react-native";
 import Carousel from "../components/Carousel";
 import { dummyData } from "../data/Data";
+
+const { width, heigth } = Dimensions.get("window");
 
 const leGrand = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
         <Carousel data={dummyData} />
+        <ScrollView>
+          <View
+            style={{
+              backgroundColor: "yellow",
+              borderRadius: 10,
+              height: 325,
+              margin: 10,
+            }}
+          >
+            <Text>Hello</Text>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
