be94abf61659247fca18c9ccfaa3cf860030f59f
unknown <chenukasumanasekara@gmail.com>
2020-09-30 00:14:27 +0530

    Fixed issues

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,25 +1,26 @@
 import * as React from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  Image,
-} from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
 
 const Map = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flexGrow: 1, height: "100%" }}>
         <View>
-          <Text style={styles.Text}>Super Markets In Colombo</Text>
+          <Text style={styles.Text}>Super Markets</Text>
         </View>
         <View>
-            <Image
-                style={{width: 405, height: 590, borderWidth: 2, borderRadius: 10, borderColor:"#fcc221"}}
-                source={require("../../assets/images/map.png")}
-            />
-        </View>     
+          <Image
+            style={{
+              width: 405,
+              height: 590,
+              borderWidth: 2,
+              borderRadius: 10,
+              borderColor: "#fcc221",
+              alignSelf: "center",
+            }}
+            source={require("../../assets/images/map.png")}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -31,8 +32,7 @@ const styles = StyleSheet.create({
   Text: {
     fontSize: 20,
     fontWeight: "bold",
-    color: "#3C4143",
     margin: 10,
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
